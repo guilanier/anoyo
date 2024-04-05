6b37eac038b23ade6dd2845fc548e5887c05273e
guilanier <lanier.gui@gmail.com>
2024-04-05 19:16:37 +0200

    add back dist as build folder

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,6 @@ import glsl from 'vite-plugin-glsl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: './docs',
-    emptyOutDir: true,
-  },
   plugins: [vue(), glsl({
     exclude: undefined, // File paths/extensions to ignore
     include: /\.(glsl|wgsl|vert|frag|vs|fs)$/i, // File paths/extensions to import
