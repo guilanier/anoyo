a796c3c0261b2eb620cf5d6661edc3e56ec0b407
guilanier <lanier.gui@gmail.com>
2024-04-12 19:42:55 +0200

    new dist

--- src/route-info.js
+++ src/route-info.js
@@ -1,7 +1,7 @@
 export const routeInfo = {
     SDFLendBlur: {
-        name: '2D SDF Lens Blur',
-        path: '/anoyo/sdf-lens-blur',
+        name: 'SDF Lens Blur',
+        path: '/sdf-lens-blur',
         componentName: 'SDFLensBlur',
     },
 };
