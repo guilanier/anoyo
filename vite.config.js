422eb33136e513c455ee2b638cddeb1c7eac5534
guilanier <lanier.gui@gmail.com>
2024-04-12 19:19:20 +0200

    new dist

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import glsl from 'vite-plugin-glsl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/anoyo/",
+  base: "/anoyo/",
   plugins: [
     vue(),
     vueJsx(),
