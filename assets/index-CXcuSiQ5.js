138029eb3b4e4292500a980b92535edbcf0bed05
guilanier <lanier.gui@gmail.com>
2024-10-29 11:48:28 +0100

    Updates

--- assets/index-CXcuSiQ5.js
+++ assets/index-CXcuSiQ5.js
@@ -1,4 +1,4 @@
-import{h as Eo,E as yo}from"./three.module-BatUJO4_.js";import"./is-tablet-2Pr9i-CQ.js";import{p as $i,i as To,I as Ao,y as bo,z as wo,r as Ro,A as xi,h as Co,H as Po,f as Lo,d as Do,o as Uo,c as Io,a as No,b as Fo,F as Oo}from"./index-auWYEPlt.js";/**
+import{h as Eo,E as yo}from"./three.module-k6Eov4rM.js";import"./is-tablet-2Pr9i-CQ.js";import{p as $i,i as To,I as Ao,y as bo,z as wo,r as Ro,A as xi,h as Co,H as Po,f as Lo,d as Do,o as Uo,c as Io,a as No,b as Fo,F as Oo}from"./index-86T8yUpr.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
